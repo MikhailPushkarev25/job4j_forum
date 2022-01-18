05c00b2eced184c764aa10191fb9ff6cb086b3ca
mikha <mixa199525@yandex.ru>
2022-01-18 19:29:36 +0300

    0. Spring test [#6881 #252120]

--- src/test/java/ru/job4j/forum/PostControlTest.java
+++ src/test/java/ru/job4j/forum/PostControlTest.java
@@ -7,11 +7,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 
@@ -29,5 +29,14 @@ public class PostControlTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("post/create"));
+    }
+
+    @Test
+    @WithMockUser
+    public void checkingPostOutputUpdate() throws Exception {
+        this.mockMvc.perform(get("/update?id=1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/update"));
     }
 }
--- src/test/java/ru/job4j/forum/PostSaveUpdateTest.java
+++ src/test/java/ru/job4j/forum/PostSaveUpdateTest.java
@@ -3,14 +3,12 @@ package ru.job4j.forum;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.job4j.forum.Main;
 import ru.job4j.forum.model.Post;
 import ru.job4j.forum.service.PostService;
 import static org.hamcrest.Matchers.is;
@@ -22,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
-public class PostSaveTest {
+public class PostSaveUpdateTest {
 
     @MockBean
     private PostService postService;
@@ -41,4 +39,5 @@ public class PostSaveTest {
         verify(postService).save(argument.capture());
         MatcherAssert.assertThat(argument.getValue().getName(), is("Куплю ладу гранту дорого"));
     }
+
 }
